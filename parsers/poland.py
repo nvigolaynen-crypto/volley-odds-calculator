53081fd008bf8a5eefc31c3358616c0ff8b83491
nvigolaynen-crypto <nvigolaynen@gmail.com>
2026-04-29 15:26:27 +0300

    Delete parsers directory

--- parsers/poland.py
+++
@@ -1,7 +0,0 @@
-from .base_parser import BaseParser
-import pandas as pd
-
-class PolandParser(BaseParser):
-    def fetch_stats(self, url: str):
-        # TODO: реализовать парсинг для Польши
-        return pd.DataFrame(), pd.DataFrame()
--- parsers/turkey.py
+++
@@ -1,7 +0,0 @@
-from .base_parser import BaseParser
-import pandas as pd
-
-class TurkeyParser(BaseParser):
-    def fetch_stats(self, url: str):
-        # TODO: реализовать парсинг для Турции
-        return pd.DataFrame(), pd.DataFrame()
