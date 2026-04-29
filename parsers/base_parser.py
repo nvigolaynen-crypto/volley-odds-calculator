31883f2cd1aea9cb5a772f6cb573ec934a876a19
nvigolaynen-crypto <nvigolaynen@gmail.com>
2026-04-29 15:47:21 +0300

    Update base_parser.py

--- parsers/base_parser.py
+++ parsers/base_parser.py
@@ -3,9 +3,4 @@ from abc import ABC, abstractmethod
 class BaseParser(ABC):
     @abstractmethod
     def fetch_stats(self, url: str):
-        """
-        Парсит одну страницу (этап) и возвращает два DataFrame:
-        - df_sets: колонки 'Команда', 'Сеты', 'Мячи'
-        - df_matches: опционально (пока не используется)
-        """
         pass
