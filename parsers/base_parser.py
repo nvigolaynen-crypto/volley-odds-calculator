9f11e72c629a441f406bbd8feb953601375bab28
nvigolaynen-crypto <nvigolaynen@gmail.com>
2026-04-30 14:39:15 +0300

    Update base_parser.py

--- parsers/base_parser.py
+++ parsers/base_parser.py
@@ -2,5 +2,11 @@ from abc import ABC, abstractmethod
 
 class BaseParser(ABC):
     @abstractmethod
-    def fetch_stats(self, url: str):
+    def fetch_stats(self, url: str, combine_phases: bool = False):
+        """Возвращает (df_stats, df_matches)"""
+        pass
+
+    @abstractmethod
+    def fetch_head_to_head(self, url: str, team1: str, team2: str):
+        """Возвращает DataFrame с историей личных встреч"""
         pass
