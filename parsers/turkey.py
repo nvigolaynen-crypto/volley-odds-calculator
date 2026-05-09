6c24a0ef1c748542e433762d598b84426c82bf23
nvigolaynen-crypto <nvigolaynen@gmail.com>
2026-05-09 18:16:20 +0300

    Update turkey.py

--- parsers/turkey.py
+++ parsers/turkey.py
@@ -1,7 +1,12 @@
-from .base_parser import BaseParser
 import pandas as pd
+from .base_parser import BaseParser
 
 class TurkeyParser(BaseParser):
-    def fetch_stats(self, url: str):
-        # TODO: реализовать парсинг для Турции
+    def fetch_stats(self, url: str, combine_phases: bool = False):
+        """Заглушка – реальный парсинг Турции будет добавлен позже"""
         return pd.DataFrame(), pd.DataFrame()
+
+    def fetch_head_to_head(self, url: str, team1: str, team2: str):
+        """Личные встречи для Турции вводятся вручную"""
+        print("[DEBUG] Личные встречи для Турции вводятся вручную")
+        return pd.DataFrame()
