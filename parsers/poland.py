d6c766edac02eb8783e94d55fb9e7189c75b11e4
nvigolaynen-crypto <nvigolaynen@gmail.com>
2026-05-09 18:17:12 +0300

    Update poland.py

--- parsers/poland.py
+++ parsers/poland.py
@@ -1,7 +1,12 @@
-from .base_parser import BaseParser
 import pandas as pd
+from .base_parser import BaseParser
 
 class PolandParser(BaseParser):
-    def fetch_stats(self, url: str):
-        # TODO: реализовать парсинг для Польши
+    def fetch_stats(self, url: str, combine_phases: bool = False):
+        """Заглушка – реальный парсинг Польши будет добавлен позже"""
         return pd.DataFrame(), pd.DataFrame()
+
+    def fetch_head_to_head(self, url: str, team1: str, team2: str):
+        """Личные встречи для Польши вводятся вручную"""
+        print("[DEBUG] Личные встречи для Польши вводятся вручную")
+        return pd.DataFrame()
