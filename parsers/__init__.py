2f8f9a5591260a601348bbc95d2a8cc8ca9520ef
nvigolaynen-crypto <nvigolaynen@gmail.com>
2026-05-11 18:10:55 +0300

    Update __init__.py

---
+++ parsers/__init__.py
@@ -0,0 +1,5 @@
+from .russia_volleyru import RussiaVolleyRuParser
+from .dataproject import DataProjectParser
+from .italy import ItalyParser
+from .poland import PolandParser
+from .turkey import TurkeyParser
