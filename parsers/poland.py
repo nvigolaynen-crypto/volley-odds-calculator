c1cadc8bcf0e7a7edfb8b44ee7ff1a8ae12c3f88
nvigolaynen-crypto <nvigolaynen@gmail.com>
2026-05-13 18:36:19 +0300

    Delete parsers/poland.py

--- parsers/poland.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from .base_parser import BaseParser
-
-class PolandParser(BaseParser):
-    def fetch_stats(self, url: str, combine_phases: bool = False):
-        return pd.DataFrame(), pd.DataFrame()
-
-    def fetch_head_to_head(self, url: str, team1: str, team2: str):
-        return pd.DataFrame()
